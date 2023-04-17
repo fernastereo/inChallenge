1e416332fb3299a2fe9cfeb2a43abc69a290d027
Fernando Cueto <fernandoecueto@gmail.com>
2023-04-17 20:45:13 +0200

    adds login and logout endpoints

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -34,5 +36,34 @@ class AuthController extends Controller
             'access_token'  => $token,
             'token_type'    => 'Bearer'
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message'       => 'Access granted',
+            'user'          => $user,
+            'access_token'  => $token,
+            'token_type'    => 'Bearer',
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()?->tokens()->delete();
+
+        return [
+            'message' => 'Logout successfull. Token was deleted.'
+        ];
     }
 }

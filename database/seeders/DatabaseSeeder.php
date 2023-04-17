a740780e11c407f400469bac6ea55ce95ca4542c
Fernando Cueto <fernandoecueto@gmail.com>
2023-04-17 22:40:00 +0200

    adds group model and sets relationship with user model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Group;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Group::factory()->create([
+            'name' => 'ADMIN',
+            'active' => true,
+        ]);
+
+        $groups = Group::factory(2)->create();
+
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@admin.com',
-        ]);
+        ])->groups()->attach(Group::all());
 
-        User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) use ($groups) {
+            $user->groups()->attach($groups->random(2));
+        });
     }
 }
